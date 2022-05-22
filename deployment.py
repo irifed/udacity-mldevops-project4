7f94d7f3d3a4eac8b60620bf5fdb7c68e065943a
Irina Fedulova <fedulova@gmail.com>
2022-05-22 16:48:35 +0300

    implemented deployment.py

--- deployment.py
+++ deployment.py
@@ -1,16 +1,9 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import shutil
 
 
-
-##################Load config.json and correct path variable
+# Load config.json and correct path variable
 with open('config.json','r') as f:
     config = json.load(f) 
 
@@ -18,10 +11,16 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
 
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
+# function for deployment
+def store_model_into_pickle():
+    # copy the latest pickle file, the latestscore.txt value,
+    # and the ingestfiles.txt file into the deployment directory
+    for fname in ['trainedmodel.pkl', 'latestscore.txt', 'ingestedfiles.txt']:
+        shutil.copy(os.path.join(dataset_csv_path, fname),
+                    os.path.join(prod_deployment_path, fname))
+
+if __name__ == "__main__":
+    store_model_into_pickle()
         
         
 
